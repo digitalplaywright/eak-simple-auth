ce6455534f1b443fd2829e1da228986b11e56d49
Andreas Sæbjørnsen <andreas@digitalplaywright.com>
2014-04-03 17:33:36 -0700

    Displaying rails validation errors

--- app/controllers/registration.js
+++ app/controllers/registration.js
@@ -8,40 +8,38 @@ var RegistrationController = Em.Controller.extend({
       _this = this;
       data  = this.getProperties("email", "password", "password_confirmation");
   
-      if (!Em.isEmpty(data.email) && !Em.isEmpty(data.password)) {
+      return Em.$.ajax({
+      
+        method: "POST",
+        url: "http://localhost:3000//users.json",
+        dataType: 'json',
+        data: {
+          user: data
+        }
+      
+      }).then((function(response) {
 
-        return Em.$.ajax({
-        
-          method: "POST",
-          url: "http://localhost:3000//users.json",
-          dataType: 'json',
-          data: {
-            user: data
-          }
-        
-        }).then((function(response) {
-
-          return Em.run((function() {
-            
-            console.log("Registration Suceeded!");
-            
-            _this.get('session').authenticate('ember-simple-auth:authenticators:oauth2', {
-              identification: data.email,
-              password: data.password
-            });
-
-            return _this.send("registrationSucceeded", response);
-
-          }));
-        
-        }), (function(xhr, status, error) {
+        return Em.run((function() {
           
-          return Em.run((function() {
-            return _this.send("registrationFailed", xhr, status, error);
-          }));
+          console.log("Registration Suceeded!");
           
+          _this.get('session').authenticate('ember-simple-auth:authenticators:oauth2', {
+            identification: data.email,
+            password: data.password
+          });
+
+          return _this.send("registrationSucceeded", response);
+
         }));
-      }
+      
+      }), (function(xhr, status, error) {
+        
+        return Em.run((function() {
+          return _this.send("registrationFailed", xhr, status, error);
+        }));
+        
+      }));
+
     },
 
     registrationSucceeded: function(response) {
@@ -49,7 +47,21 @@ var RegistrationController = Em.Controller.extend({
     },
 
     registrationFailed: function(xhr, status, error) {
-      return console.log("Registration Failed: " + error);
+
+      var cur_error  = JSON.parse(xhr.responseText)["errors"];
+
+      var cur_error_str = "<div class='regerror error'>";
+      for (var key in cur_error) {
+        if (cur_error.hasOwnProperty(key)) {
+          cur_error_str = cur_error_str + key + " " + cur_error[key];
+          cur_error_str = cur_error_str + "<br/>"
+        }
+      }
+
+      cur_error_str = cur_error_str + "</div>"
+
+      $(".regerror").replaceWith(cur_error_str);
+      return console.log("Registration Failed: " + status);
     }
 
   }
