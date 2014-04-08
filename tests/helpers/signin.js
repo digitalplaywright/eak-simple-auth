5fcaa022301d88e7505b398485421b9e5979a045
Andreas Sæbjørnsen <andreas@digitalplaywright.com>
2014-04-07 19:53:18 -0700

    added missing files

---
+++ tests/helpers/signin.js
@@ -0,0 +1,34 @@
+
+function signInUser(app, userPayload) {
+  userPayload = $.extend({
+    email: 'test_user@example.com',
+    accessToken: 'please',
+  }, userPayload || {});
+
+  var session = app.__container__.lookup('ember-simple-auth:session');
+  var store   = app.__container__.lookup('store:main');
+
+  return session.authenticate('authenticator:application', {
+    email: userPayload.email,
+    accessToken: userPayload.accessToken,
+  }).then(wait);
+}
+
+export function setEphemeralSessionStore(app) {
+  var session   = app.__container__.lookup('ember-simple-auth:session');
+  console.log("in setEphemeralSessionStore");
+  console.log(session);
+  session.store = Ember.SimpleAuth.Stores.Ephemeral.create();
+
+  app.deferReadiness();
+  session.invalidate().then(function () {
+    app.advanceReadiness();
+  }, "invalidating test session");
+}
+
+
+var registerAsyncHelper = Ember.Test.registerAsyncHelper;
+
+export function install() {
+ registerAsyncHelper('signInUser', signInUser);
+}
