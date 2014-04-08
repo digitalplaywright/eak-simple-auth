b49ffffb8718b5d3aa6429747dc1428438adbaf1
Andreas Sæbjørnsen <andreas@digitalplaywright.com>
2014-04-07 23:24:51 -0700

    remove debug code

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -12,13 +12,8 @@ module('Acceptances - SignIn', {
 });
 
 test('see if we can sign in successfully', function(){
-    console.log("App is:");
-
     var session = App.__container__.lookup('ember-simple-auth:session');
 
-    console.log(session.authenticate);
-
-    //alert(console.log(App) );
     session.authenticate('authenticator:application', {
       email:  'a@b.com',
       accessToken: 'something',
