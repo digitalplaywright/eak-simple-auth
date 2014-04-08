2dfcd513b6d2e2617243c347f9a2002d219a4385
Andreas Sæbjørnsen <andreas@digitalplaywright.com>
2014-04-07 23:22:38 -0700

    remove commented out alternate code path

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -35,35 +35,3 @@ test('see if we can sign in successfully', function(){
       equal(currentURL(), '/protected' , "/we are not redirected when we are already signed in");
     });
 });
-
-//function makeEmail() { 
-//    var strValues="abcdefg12345"; 
-//    var strEmail = ""; 
-//    var strTmp; 
-//    for (var i=0;i<10;i++) { 
-//        strTmp = strValues.charAt(Math.round(strValues.length*Math.random())); 
-//        strEmail = strEmail + strTmp; 
-//    } 
-//    strTmp = ""; 
-//    strEmail = strEmail + "@"; 
-//    for (var j=0;j<8;j++) { 
-//        strTmp = strValues.charAt(Math.round(strValues.length*Math.random())); 
-//        strEmail = strEmail + strTmp; 
-//    } 
-//    strEmail = strEmail + ".com" 
-//    return strEmail; 
-//}  
-//test('authentication works correctly', function() {
-//  visit('/').then(function() {
-//    ok(exists('a:contains(Login)'), 'Login button is displayed when not authenticated');
-//    ok(!exists('a:contains(Logout)'), 'Logout button is not displayed when not authenticated');
-//  });
-//  visit('/login').fillIn('#identification', makeEmail()).fillIn('#password', 'this-is-wrong!').click('button[type="submit"]').then(function() {
-//    ok(exists('a:contains(Login)'), 'Login button is displayed when not authenticated');
-//    ok(!exists('a:contains(Logout)'), 'Logout button is not displayed when not authenticated');
-//  });
-//  visit('/login').fillIn('#identification', makeEmail()).fillIn('#password', 'password').click('button[type="submit"]').then(function() {
-//    ok(!exists('a:contains(Login)'), 'Login button is not displayed when authenticated');
-//    ok(exists('a:contains(Logout)'), 'Logout button is displayed when authenticated');
-//  });
-//});
